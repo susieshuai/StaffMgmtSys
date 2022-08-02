e882c7d9291fa5045b6957e4386ca646e462cb08
shuais <shuai.s@northeastern.edu>
2022-08-01 21:39:27 -0700

    create worker manager class

---
+++ workerManager.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Shi Shuai on 2022/8/1.
+//
+
+#include "workerManager.h"
+
+WorkerManager::WorkerManager() {
+
+}
+
+WorkerManager::~WorkerManager() {
+
+}
---
+++ workerManager.h
@@ -0,0 +1,18 @@
+//
+// Created by Shi Shuai on 2022/8/1.
+//
+
+#ifndef STAFFMGMTSYS_WORKERMANAGER_H
+#define STAFFMGMTSYS_WORKERMANAGER_H
+
+#endif //STAFFMGMTSYS_WORKERMANAGER_H
+
+#pragma once
+#include <iostream>
+using namespace std;
+
+class WorkerManager{
+public:
+    WorkerManager();
+    ~WorkerManager();
+};
